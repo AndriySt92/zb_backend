6b8fe117ca3977b9679196b44862f8f60d43c692
AndriySt92 <andriyua-1992@mail.ru>
2024-05-03 22:48:49 +0300

    Fix vercel

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import cors from 'cors'
 import connectDB from './db/connectDb.js'
 import userRouter from './routes/userRouter.js'
 import dealsRouter from './routes/dealsRouter.js'
-import dealsModel from './models/dealsModel.js'
 
 const app = express()
 dotenv.config()
@@ -18,16 +17,9 @@ app.use(express.json())
 app.use('/api/auth', userRouter)
 app.use('/api/deals', dealsRouter)
 
-app.get('/', async (req, res) => {
-  try {
-    const deals = await dealsModel.find().sort('-createdAt')
-
-    res.json(deals)
-  } catch (error) {
-    console.error('Error in get all deals:', error)
-    res.status(500).json({ error: 'Internal server error' })
-  }
-},)
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Work'})
+})
 
 app.listen(PORT, () => {
   console.log(`server is listening on ${PORT}`)
